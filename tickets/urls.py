6636b51f97b6b73dc43e1ed2bd9bfe293eaefee7
fatemeh <fatemeh@gmail.com>
2025-10-04 13:42:36 +0330

    fetch

---
+++ tickets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('tickets', index, name='tickets'),
+    path('tickets/create', ticket_create, name='tickets-create'),
+    path('tickets/<int:id>/detail', ticket_detail, name='tickets-detail'),
+    path('tickets/edit/<int:id>', ticket_edit, name='tickets-update'),
+    path('tickets/delete/<int:id>', ticket_delete, name='tickets-delete'),
+
+    path('change/mode', color_mode, name="color_mode"),
+
+]
