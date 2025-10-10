327bfe2530d7fbd5d7112f40f3b001b9c98b50a9
fatemeh <fatemeh@gmail.com>
2025-10-10 17:12:18 +0330

    fetch: add validate form & age & email

--- tickets/forms.py
+++ tickets/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ["category", "priority", "subject", "description", "tags", "max_reply_date"]
+        fields = ["category", "priority", "subject", "description", "tags", "max_reply_date", "age", "email"]
 
         widgets = {
             "subject": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter subject"}),
@@ -13,5 +13,7 @@ class TicketForm(forms.ModelForm):
             "priority": forms.Select(attrs={"class": "form-select"}),
             "category": forms.Select(attrs={"class": "form-select"}),
             "tags": forms.SelectMultiple(attrs={"class": "form-select"}),
-            "max_reply_date": forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"})
+            "max_reply_date": forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"}),
+            "age": forms.IntegerField(required=True),
+            "email": forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
         }
