e20f7731277e409d3a6bf6301c2075131fd5b0e4
fatemeh <fatemeh@gmail.com>
2025-10-18 12:05:59 +0330

    fetch: add choices

---
+++ tickets/choices.py
@@ -0,0 +1,27 @@
+
+
+PRIORITY_CHOICES = [
+    ("low", "Low"),
+    ("middle", "Middle"),
+    ("high", "High"),
+]
+
+PRIORITY_COLORS = {
+    "low": "#198754",
+    "middle": "#0d6efd",
+    "high": "#dc3545",
+}
+
+STATUS_CHOICES = [
+    ("new", "New"),
+    ("in-progress", "In Progress"),
+    ("solved", "Solved"),
+    ("impossible", "Impossible"),
+]
+
+STATUS_COLORS = {
+    "new": "#0dcaf0",
+    "in-progress": "#0d6efd",
+    "solved": "#198754",
+    "impossible": "#dc3545",
+}
