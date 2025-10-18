aaab537404029f9775e3d3d6dfeaae6732c2e721
fatemeh <fatemeh@gmail.com>
2025-10-18 12:02:27 +0330

    fetch: add highlight

---
+++ tickets/templatetags/highlight.py
@@ -0,0 +1,18 @@
+import re
+from distutils.command.register import register
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter
+def highlight(text, search):
+    if not search:
+        return text
+    pattern = re.compile(re.escape(search), re.IGNORECASE)
+    return mark_safe(pattern.sub(
+        lambda m: f'<mark>{m.group(0)}<mark>',
+        str(text)
+    ))
