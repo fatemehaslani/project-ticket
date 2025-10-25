99ab2799839306af4b725296eb8d20e76646d174
fatemeh <fatemeh@gmail.com>
2025-10-25 14:54:45 +0330

    fetch: comment

--- tickets/urls.py
+++ tickets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('tickets/edit/<int:id>', ticket_edit, name='tickets-update'),
     path('tickets/delete/<int:id>', ticket_delete, name='tickets-delete'),
 
-    path('change/mode', color_mode, name="color_mode"),
+   # path('change/mode', color_mode, name="color_mode"),
 
 ]
