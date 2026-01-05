f6bc4b08403df39d73349e081a523c36597fa0de
fatemeh <fatemeh@gmail.com>
2026-01-05 14:33:12 +0330

    fetch: add admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from tickets.models import *
+
 # Register your models here.
+
+admin.site.register(SearchLog)
+admin.site.register(Ticket)
+admin.site.register(Category)
+admin.site.register(Tag)
